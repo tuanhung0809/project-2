40a4ee9f01bb345379eb8fb822af55b2faa0907b
Phan Việt Hưng <phanviethung@Lod-Mac.local>
2020-08-21 20:41:47 +0700

    add listProduct

--- script2.js
+++ script2.js
@@ -11,32 +11,35 @@ $(".album-poster").on('click', function(e){
 
 
 
+
+// console.log(ap);
+
+listMusic = [
+    {
+    name: 'OK',
+    artist: 'BINZ',
+    url: '01 Ok.m4a',
+    cover: 'https://i.ytimg.com/vi/SNES5Y-tYxM/hqdefault.jpg'
+},
+    {
+    name: 'HAI CHU DA TUNG',
+    artist: 'NHU VIET',
+    url: 'Hai Chu Da Tung - Nhu Viet_ ACV.flac',
+    cover: 'https://data.chiasenhac.com/data/cover/126/125532.jpg'
+},
+    {
+    name: 'CON TRAI CUNG',
+    artist: 'BRAY, K-ICM',
+    url: 'Con Trai Cung - K-ICM_ B Ray.m4a',
+    cover: 'https://data.chiasenhac.com/data/cover/98/97679.jpg'
+},
+
+]
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     listFolded: true,
 
-    audio: [
-        {
-        name: 'OK',
-        artist: 'BINZ',
-        url: '01 Ok.m4a',
-        cover: 'https://i.ytimg.com/vi/SNES5Y-tYxM/hqdefault.jpg'
-    },
-        {
-        name: 'HAI CHU DA TUNG',
-        artist: 'NHU VIET',
-        url: 'Hai Chu Da Tung - Nhu Viet_ ACV.flac',
-        cover: 'https://data.chiasenhac.com/data/cover/126/125532.jpg'
-    },
-        {
-        name: 'CON TRAI CUNG',
-        artist: 'BRAY, K-ICM',
-        url: 'Con Trai Cung - K-ICM_ B Ray.m4a',
-        cover: 'https://data.chiasenhac.com/data/cover/98/97679.jpg'
-    },
-
-    ]
+    audio: listMusic,
 });
 
-// console.log(ap);
 
