b35e539bccbd8755a4cb5f8631f8db17b5ea51ce
Phan Việt Hưng <phanviethung@Lod-Mac.local>
2020-08-25 19:17:17 +0700

    new update

--- script2.js
+++ script2.js
@@ -1,20 +1,14 @@
 
 // Click songs to get id
 
-$(".album-poster").on('click', function(e){
-    var dataSwitchedId = $(this).attr('data-switch');
 
-    ap.list.switch(dataSwitchedId); //Change songs
-    ap.play()
-    $('#aplayer').addClass('showPlayer')
-});
 
 
 
 
 // console.log(ap);
 
-listMusic = [
+let listMusic = [
     {
     name: 'OK',
     artist: 'BINZ',
@@ -33,8 +27,48 @@ listMusic = [
     url: 'Con Trai Cung - K-ICM_ B Ray.m4a',
     cover: 'https://data.chiasenhac.com/data/cover/98/97679.jpg'
 },
-
+{
+    name: 'CON TRAI CUNG',
+    artist: 'BRAY, K-ICM',
+    url: 'Con Trai Cung - K-ICM_ B Ray.m4a',
+    cover: 'https://data.chiasenhac.com/data/cover/98/97679.jpg'
+},
 ]
+
+function displayMusic(listMusic) {
+    let domMusics = document.getElementById('musicNewRelease');
+    domMusics.innerHTML = '';
+    let totalHTML = '<div class="row">'
+    for (let i = 0; i < listMusic.length; i++) {
+        const music = listMusic[i]
+        let html = `
+        <div class="col-md-3">
+            <a href="javascript:void();" class="album-poster" data-switch="${i}">
+                <img src="${music['cover']}" alt="">
+            </a>
+            <h4>${music['name']}</h4>
+            <p>${music['artist']}</p>
+        </div>`;
+        totalHTML += html;
+        if (i%4 === 3) {
+            totalHTML += `</div><div class="row">`;
+        }
+    }
+    totalHTML += `</div>`;
+    domMusics.innerHTML = totalHTML;
+}
+
+displayMusic(listMusic)
+
+
+$(".album-poster").on('click', function(e){
+    var dataSwitchedId = $(this).attr('data-switch');
+    // console.log(dataSwitchedId);
+    ap.list.switch(dataSwitchedId); //Change songs
+    ap.play()
+    $('#aplayer').addClass('showPlayer')
+});
+
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     listFolded: true,
@@ -42,4 +76,15 @@ const ap = new APlayer({
     audio: listMusic,
 });
 
+function searchSongs() {
+    let result = listMusic.filter(function(listMusic){
+        return listMusic.name.includes(document.getElementById('searchBar').value)
+    })
+    displayMusic(result)
+}
 
+function searchSongsEnter() {
+    if (event.key === 'Enter') {
+        searchSongs()
+    }
+}
