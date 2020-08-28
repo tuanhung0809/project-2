ae8c317dc344bd707180cdfdd34871ef42edb303
Phan Việt Hưng <phanviethung@Lod-Mac.local>
2020-08-28 12:37:45 +0700

    add genre and artist

--- script2.js
+++ script2.js
@@ -5,257 +5,303 @@ let listMusic =[
     name: 'ok',
     artist: 'binz',
     url: 'music/01 Ok.m4a',
-    cover: 'https://i.ytimg.com/vi/SNES5Y-tYxM/hqdefault.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/115/114164.jpg',
+    genre: 'Rap'
 },
     {
     name: 'hai chu da tung',
     artist: 'nhu viet',
     url: 'music/Hai Chu Da Tung - Nhu Viet_ ACV.flac',
-    cover: 'https://data.chiasenhac.com/data/cover/126/125532.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/126/125532.jpg',
+    genre: 'Ballad'
 },
     {
     name: 'con trai cưng',
     artist: 'bray, k-icm',
     url: 'music/Con Trai Cung - K-ICM_ B Ray.m4a',
-    cover: 'https://data.chiasenhac.com/data/cover/98/97679.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/98/97679.jpg',
+    genre: 'Rap'
 },
     {
     name: 'trời hôm nay nhiều mây cực',
     artist: 'đen',
     url: 'music/Troi Hom Nay Nhieu May Cuc_ - Den.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/126/125234.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/126/125234.jpg',
+    genre: 'Rap'
 },
     {
     name: 'bài này chill phết',
     artist: 'đen',
     url: 'music/Bai Nay Chill Phet - Den.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/105/104834.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/105/104834.jpg',
+    genre: 'Rap'
 },
     {
     name: 'anh dếch cần gì nhiều ngoài em',
     artist: 'đen, vũ, thành đồng',
     url: 'music/Anh Dech Can Gi Nhieu Ngoai Em - Den_ Vu.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/98/97369.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/98/97369.jpg',
+    genre: 'Rap'
 },
         {
     name: 'vài lần đón đưa',
     artist: 'soobin hoàng sơn, touliver',
     url: 'music/Vai Lan Don Dua - Soobin Hoang Son_ Toul.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/75/74828.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/75/74828.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'phía sau một cô gái',
     artist: 'soobin hoàng sơn',
     url: 'music/Phia Sau Mot Co Gai - Soobin Hoang Son.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/65/64657.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/65/64657.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'xin đừng lặng im',
     artist: 'soobin hoàng sơn',
     url: 'music/Xin Dung Lang Im - Soobin Hoang Son.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/75/74260.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/75/74260.jpg',
+    genre: 'Ballad'
 },      
     {
     name: 'chuyến đi của năm (đi để trở về 2) beat',
     artist: 'soobin hoàng sơn',
     url: 'music/Chuyen Di Cua Nam Di De Tro Ve 2_ - Soob.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/82/81161.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/82/81161.jpg',
+    genre: 'Ballad'
 },
     {
     name: 'sóng gió',
     artist: 'k-icm; jack',
     url: 'music/Song Gio - Jack_ K-ICM.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/120/119856.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/120/119856.jpg',
+    genre: 'Ballad'
 },
     {
     name: 'hồng nhan',
     artist: 'jack',
     url: 'music/Hong Nhan - Jack.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/105/104211.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/105/104211.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'bạc phận',
     artist: 'jack; k-icm',
     url: 'music/Bac Phan - Jack_ K-ICM.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/103/102994.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/103/102994.jpg',
+    genre: 'Ballad'
 },
         {
     name: 'có chắc yêu là đây',
     artist: 'sơn tùng m-tp',
     url: 'music/Co Chac Yeu La Day - M-TP.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/124/123411.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/124/123411.jpg',
+    genre: 'Ballad'
 },
     {
     name: 'nơi này có anh',
     artist: 'sơn tùng m-tp',
     url: 'Noi Nay Co Anh - Son Tung M-TP.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/87/86272.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/87/86272.jpg',
+    genre: 'Ballad'
     },
 {
     name: 'em không sai chúng ta sai',
     artist: 'erik',
     url: 'music/Em Khong Sai Chung Ta Sai - Erik.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/121/120248.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/121/120248.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'sau tất cả',
     artist: 'erik',
     url: 'music/Sau Tat Ca - Erik.mp3',
-    cover: 'https://data.chiasenhac.com/data/artist_avatar/3/2116.jpg'
+    cover: 'https://data.chiasenhac.com/data/artist_avatar/3/2116.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'ghen',
-    artist: 'erik; Mmn',
+    artist: 'erik; min',
     url: 'music/Ghen - Erik_ Min.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/72/71762.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/72/71762.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'big city boi',
     artist: 'binz; touliver',
     url: 'music/Big City Boi - Binz_ Touliver.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/124/123404.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/124/123404.jpg',
+    genre: 'Rap'
 },
 {
     name: 'ánh nắng của anh',
     artist: 'đức phúc',
     url: 'music/Anh Nang Cua Anh - Duc Phuc.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/69/68690.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/69/68690.jpg',
+    genre: 'Ballad'
 },
 {
     name: 'hơn cả yêu',
     artist: 'đức phúc',
     url: 'music/Hon Ca Yeu - Duc Phuc.mp3',
-    cover: 'https://data.chiasenhac.com/data/cover/116/115822.jpg'
+    cover: 'https://data.chiasenhac.com/data/cover/116/115822.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Cô Đơn Không Muốn Về Nhà',
     artist:'Mr Siro',
     url: 'music/Co Don Khong Muon Ve Nha - Mr Siro.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/115/114493.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/115/114493.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Do For Love',
     artist:'B Ray_ AMee_ Masew',
     url: 'music/Do For Love - B Ray_ AMee_ Masew.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/117/116090.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/117/116090.jpg',
+    genre: 'Rap'
 },
 {
     name:'2 Phut Hon',
     artist:' Phao',
     url: 'music/2 Phut Hon - Phao.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/117/116813.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/117/116813.jpg',
+    genre: 'Rap'
 },
 {
     name:'Duyen Am ',
     artist:'Hoang Thuy Linh',
     url: 'music/Duyen Am - Hoang Thuy Linh.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/111/110870.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/111/110870.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Hanh Lang Cu Ha Nho',
     artist:' Long Non La x Masew',
     url: 'music/Hanh Lang Cu Ha Nho_ - Long Non La x Mas.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/115/114815.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/115/114815.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Em có nghe',
     artist:'Kha',
     url: 'music/Em Co Nghe - Kha.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/117/116629.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/117/116629.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Khong-Yeu Dung Gay Thuong Nho',
     artist:'LyLy-Karik',
     url: 'music/Khong-Yeu-Dung-Gay-Thuong-Nho-LyLy-Karik.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/120/119821.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/120/119821.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Khoc Cung Em',
     artist:'Mrsiro - Gray - WinD',
     url: 'music/Khoc Cung Em - Mr_Siro_ Gray_ Wind [128kbps_MP3].mp3',
-    cover:'https://data.chiasenhac.com/data/cover/117/116275.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/117/116275.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Sao Anh Chua Ve Nha',
     artist:'AMEE - Ricky Star',
     url: 'music/Sao Anh Chua Ve Nha - AMEE_ Ricky Star.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/118/117057.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/118/117057.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Sợ rằng em biết anh còn yêu em',
     artist:'Juun đặng dũng',
     url: 'music/SoRangEmBietAnhConYeuEm-JuunDangDung-6217224.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/116/115678.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/116/115678.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Xa - Masew; APJ',
     artist:'Masew',
     url: 'music/Xa - Masew_ APJ.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/100/99010.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/100/99010.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Đủ Xa Tình Sẽ Cũ',
     artist:'JSOL',
     url: 'music/Du Xa Tinh Se Cu - JSOL.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/102/101521.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/102/101521.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Khong Sao Ma Em Day Roi',
     artist:'Suni Ha Linh',
     url: 'music/Khong Sao Ma Em Day Roi - Suni Ha Linh_.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/126/125572.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/126/125572.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Đố Em Biết Anh Đang Nghĩ Gì',
     artist:'JustaTee; Đen; Biên',
     url: 'music/Do Em Biet Anh Dang Nghi Gi - JustaTee_.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/95/94418.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/95/94418.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Hai Triệu Năm',
     artist:'Đen; Biên',
     url: 'music/Hai Trieu Nam - Den_ Bien.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/107/106262.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/107/106262.jpg',
+    genre: 'Rap'
 },
 {
     name:'3 1 0 7',
     artist:'Duongg; Nâu',
     url: 'music/3 1 0 7 - Duongg_ Nau.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/112/111664.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/112/111664.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Một Nhà',
     artist:'Da LAB',
     url: 'music/Mot Nha - Da LAB.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/35/34197.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/35/34197.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Gác Lại Âu Lo',
     artist:'Da LAB; Miu Lê',
     url: 'music/Gac Lai Au Lo - Da LAB_ Miu Le.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/125/124649.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/125/124649.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Lạc Trôi',
     artist:'Sơn Tùng M-TP',
     url: 'music/Lac Troi - Son Tung M-TP.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/87/86272.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/87/86272.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Cố Giang Tình',
     artist:'X2X',
     url: 'music/Co Giang Tinh - X2X.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/119/118265.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/119/118265.jpg',
+    genre: 'Ballad'
 },
 {
     name:'Thằng Điên',
     artist:'JustaTee; Phương Ly',
     url: 'music/Thang Dien - JustaTee_ Phuong Ly.mp3',
-    cover:'https://data.chiasenhac.com/data/cover/96/95803.jpg'
+    cover:'https://data.chiasenhac.com/data/cover/96/95803.jpg',
+    genre: 'Ballad'
 },
 
 ]
 
+for (let i = 0; i < listMusic.length; i++) {
+    listMusic[i].artist = listMusic[i].artist.toLowerCase()
+    listMusic[i].name = listMusic[i].name.toLowerCase()
+}
 
 function displayMusic(listMusic) {
     let domMusics = document.getElementById('musicNewRelease');
@@ -279,38 +325,6 @@ function displayMusic(listMusic) {
     totalHTML += `</div>`;
     domMusics.innerHTML = totalHTML;
 }
-
-// function displayMusic(listMusic,genre) {
-//     let domMusics = document.getElementById('main-body');
-//     domMusics.innerHTML = '';
-//     let totalHTML = `
-//     <div class="row">
-//         <div class="col-md-12">
-//             <h2>${genre}</h2>
-//         </div>
-//     </div>
-//     <div class="row">
-//     `
-//     for (let i = 0; i < listMusic.length; i++) {
-//         const music = listMusic[i]
-//         let html = `
-//         <div class="col-md-3">
-//             <a href="javascript:void();" class="album-poster" data-switch="${i}">
-//                 <img src="${music['cover']}" alt="">
-//             </a>
-//             <h4>${music['name']}</h4>
-//             <p>${music['artist']}</p>
-//         </div>`;
-//         totalHTML += html;
-//         if (i%4 === 3) {
-//             totalHTML += `</div><div class="row">`;
-//         }
-//     }
-//     totalHTML += `</div>`;
-//     domMusics.innerHTML = totalHTML;
-// }
-
-
 
 
 displayMusic(listMusic,'Recommended Musics')
@@ -337,11 +351,8 @@ const ap = new APlayer({
 function searchSongs() {
     let result = listMusic.filter(function(listMusic){
         return listMusic.name.includes(document.getElementById('searchBar').value);
-        
-        
     })
-    // console.dir(result);
-    
+ 
     displayMusic(result,'Search Result');
     ap.list.clear();
     ap.list.add(result);
@@ -352,9 +363,30 @@ function searchSongsEnter(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
         searchSongs();
-        
-
     }
 }
 
+function showGenre(n) {
+    let genre = n.innerHTML
+    let result = listMusic.filter(function(listMusic){
+        return listMusic.genre.includes(genre);
+    })
+    displayMusic(result,genre);
 
+    ap.list.add(result);
+}
+
+
+function showArtist(n) {
+    let artist = n.innerHTML.toLowerCase()
+    console.log(artist);
+    let result = listMusic.filter(function(listMusic){
+        return listMusic.artist.includes(artist);
+    })
+    displayMusic(result,artist);
+    ap.list.add(result);
+}
+
+function returnHome() {
+    displayMusic(listMusic,'Recommended Musics')
+}
